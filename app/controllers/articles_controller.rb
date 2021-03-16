77d5ee6d59aa010d4317cb22bb082ede51c2a81b
谷村有寿麻 <t.azumax0502@gmail.com>
2021-03-16 22:00:11 +0900

    記事の作成、エラー文、flash作成。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: '記事を投稿しました'
+    else
+      flash.now[:alert] = '記事を投稿出来ませんでした'
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
